a176618d0f2be1534f4a87fe3683deb3a511d945
hsx0306 <1317hsb@gmail.com>
2025-05-20 15:21:24 +0900

    Initial commit
    초기세팅

---
+++ src/main/java/com/sch/groomthon/hsx0306/Hsx0306Application.java
@@ -0,0 +1,13 @@
+package com.sch.groomthon.hsx0306;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hsx0306Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hsx0306Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sch/groomthon/hsx0306/Hsx0306ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sch.groomthon.hsx0306;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hsx0306ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
